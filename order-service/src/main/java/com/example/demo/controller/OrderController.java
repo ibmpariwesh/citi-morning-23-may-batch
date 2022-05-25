4568b847f7169fb2c7beb95d1914a3bca5e62977
ibmpariwesh <pariweshg@gmail.com>
2022-05-25 11:27:27 +0530

    added config server

--- order-service/src/main/java/com/example/demo/controller/OrderController.java
+++ order-service/src/main/java/com/example/demo/controller/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,17 +22,18 @@ import com.example.demo.service.OrderService;
 import com.example.demo.vo.OrderVO;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
 	@Autowired
 	OrderService service;
 	
-	@PostMapping("/order")
+	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	Integer createOrder(@Valid @RequestBody OrderVO order) {
 		System.out.println(order.getItem());
 		return service.saveOrder(order);
 	}
-	@GetMapping("/order")
+	@GetMapping
 	List<OrderVO> getOrders() {
 		return service.getOrders();
 	}

0e1d17b9f6a5ec24ffe6733d14995ca762b47f4f
ibmpariwesh <pariweshg@gmail.com>
2022-05-25 13:43:17 +0530

    unit testing controllers- getorders

--- order-service/src/test/java/com/example/demo/controller/OrderControllerTest.java
+++ order-service/src/test/java/com/example/demo/controller/OrderControllerTest.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -39,7 +42,11 @@ class OrderControllerTest {
 
 	@Test
 	void testGetOrders() {
-		fail("Not yet implemented");
+		List<OrderVO> orders = new ArrayList<OrderVO>();
+		OrderVO order1 = new OrderVO();
+		when(service.getOrders()).thenReturn(orders);
+		List<OrderVO> response = controller.getOrders();
+		assertEquals(1, response.size());
 	}
 
 	@Test
